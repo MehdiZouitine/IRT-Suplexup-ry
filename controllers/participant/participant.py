539b5561f3d4d85d4a41d70a678488dcaf16c718
David Bertoin <davidbertoin@gmail.com>
2023-05-15 15:13:17 +0200

    Deterministic behaviour

--- controllers/participant/participant.py
+++ controllers/participant/participant.py
@@ -12,28 +12,88 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Minimalist controller example for the Robot Wrestling Tournament.
-   Demonstrates how to play a simple motion file."""
+"""
+Demonstrates the gait manager (inverse kinematics + simple ellipsoid path).
+"""
 
 from controller import Robot
 import sys
-
-# We provide a set of utilities to help you with the development of your controller. You can find them in the utils folder.
-# If you want to see a list of examples that use them, you can go to https://github.com/cyberbotics/wrestling#demo-robot-controllers
 sys.path.append('..')
-from utils.motion_library import MotionLibrary
+# Eve's locate_opponent() is implemented in this module:
+from utils.image_processing import ImageProcessing as IP
+from utils.fall_detection import FallDetection
+from utils.gait_manager import GaitManager
+# from utils.camera import Camera
 
 
-class Wrestler (Robot):
+class Fatima (Robot):
+    SMALLEST_TURNING_RADIUS = 0.1
+    SAFE_ZONE = 0.75
+    TIME_BEFORE_DIRECTION_CHANGE = 200  # 8000 ms / 40 ms
+
+    def __init__(self):
+        Robot.__init__(self)
+        self.time_step = int(self.getBasicTimeStep())
+
+        # self.camera = Camera(self)
+        self.fall_detector = FallDetection(self.time_step, self)
+        self.gait_manager = GaitManager(self, self.time_step)
+        self.heading_angle = 3.14 / 2
+        # Time before changing direction to stop the robot from falling off the ring
+        self.counter = 0
+        self.t = 0
+        self.start = True
+
     def run(self):
-        # to load all the motions from the motions folder, we use the MotionLibrary class:
-        motion_library = MotionLibrary()
-        # retrieves the WorldInfo.basicTimeTime (ms) from the world file
-        time_step = int(self.getBasicTimeStep())
-        while self.step(time_step) != -1:  # mandatory function to make the simulation run
-            motion_library.play('Backwards')
+        
+        n = 120
+        while self.step(self.time_step) != -1:
+            self.fall_detector.check()
+            if self.t < 20:
+                radius=0.05
+            elif self.t in range(n, n+35):
+                radius = -0.05
+            elif self.t in range(n+125, n+155):
+                radius = -0.05
+            elif self.t >n+215:
+                print("=ok=")
+                radius = -0.05
+            else:
+                radius = 0
+            
+            self.gait_manager.update_theta()
+            self.gait_manager.command_to_motors(desired_radius=radius)
+            self.t += 1
+            # self.gait_manager.command_to_motors(desired_radius=radius)
+            # if self.start:
+                # radius=-0.1
+            # else:
+                # if self.t < 30:
+                    # radius=0.1
+                # elif (self.t > 30):
+                    # radius=0
+            
+            # We need to update the internal theta value of the gait manager at every step:
+            # self.gait_manager.update_theta()
+            # self.gait_manager.command_to_motors(desired_radius=radius)
+            # self.t += 1
+            # if self.t == 100:
+                # self.t = 0
+            # if self.t == 30:
+                # self.start = False
+                
+
+    
+
+    def _get_normalized_opponent_x(self):
+        """Locate the opponent in the image and return its horizontal position in the range [-1, 1]."""
+        img = self.camera.get_image()
+        _, _, horizontal_coordinate = IP.locate_opponent(img)
+        if horizontal_coordinate is None:
+            return 0
+        return horizontal_coordinate * 2 / img.shape[1] - 1
 
 
 # create the Robot instance and run main loop
-wrestler = Wrestler()
+wrestler = Fatima()
 wrestler.run()
